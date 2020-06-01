cf68f63074f1e2b8dfff62209bdf02b8deae5752
Matija Stepanic <stepanic.matija@gmail.com>
2020-06-01 17:58:34 +0200

    Setup CLI

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { program } = require('commander');
+
 const fs = require('fs');
 
 // Material pallete 700 or 800 or 900 for white text
@@ -29,101 +31,120 @@ const colorByIndex = (id) => {
     return colorsArray[index];
 }
 
-console.log('Converting: _SCHEMA.json to DBML');
+const convert = (schemaJSON, schemaDBML) => {
+    console.log(`Converting: ${schemaJSON} to ${schemaDBML}`);
 
-// read MongoDB _SCHEMA collection from _SCHEMA.json as JSON array of objects
-const _SCHEMA = JSON.parse(fs.readFileSync('./_SCHEMA.json', 'utf-8'));
+    // read MongoDB _SCHEMA collection from _SCHEMA.json as JSON array of objects
+    const _SCHEMA = JSON.parse(fs.readFileSync(schemaJSON, 'utf-8'));
 
-// console.log(_SCHEMA);
+    // console.log(_SCHEMA);
 
-let classIndex = 0;
+    let classIndex = 0;
 
-let DBML = [];
+    let DBML = [];
 
-_SCHEMA.forEach(parseClass => {
+    _SCHEMA.forEach(parseClass => {
 
-    const className = parseClass._id;
+        const className = parseClass._id;
 
-    console.log(className);
+        // console.log(className);
 
-    const keys = Object.keys(parseClass);
+        const keys = Object.keys(parseClass);
 
-    const fields = keys.filter(key => {
+        const fields = keys.filter(key => {
 
-        // Remove specific Parse keys (keep only custom fields)
-        if (!['_id', 'objectId', 'updatedAt', 'createdAt', '_metadata'].includes(key)) {
-            return key;
-        }
-    });
+            // Remove specific Parse keys (keep only custom fields)
+            if (!['_id', 'objectId', 'updatedAt', 'createdAt', '_metadata'].includes(key)) {
+                return key;
+            }
+        });
 
-    const color = colorByIndex(classIndex);
+        const color = colorByIndex(classIndex);
 
-    let TABLE = `Table ${className} [headercolor: ${color}] {
+        let TABLE = `Table ${className} [headercolor: ${color}] {
     objectId string
 `;
 
-    const scalarFields = [];
-    const pointerFields = [];
-    const relationFields = [];
+        const scalarFields = [];
+        const pointerFields = [];
+        const relationFields = [];
 
-    const indexes = [];
-    indexes.push(`      objectId [pk]`);
+        const indexes = [];
+        indexes.push(`      objectId [pk]`);
 
-    fields.forEach(fieldName => {
-        const fieldType = parseClass[fieldName];
-        // console.log(fieldName, fieldType);
+        fields.forEach(fieldName => {
+            const fieldType = parseClass[fieldName];
+            // console.log(fieldName, fieldType);
 
-        if (fieldType.startsWith('*')) {
-            pointerFields.push(fieldName);
-        } else if (fieldType.startsWith('relation<')) {
-            relationFields.push(fieldName);
-        } else {
-            scalarFields.push(fieldName);
-        }
-    });
+            if (fieldType.startsWith('*')) {
+                pointerFields.push(fieldName);
+            } else if (fieldType.startsWith('relation<')) {
+                relationFields.push(fieldName);
+            } else {
+                scalarFields.push(fieldName);
+            }
+        });
 
-    scalarFields.forEach(fieldName => {
-        const fieldType = parseClass[fieldName];
-        TABLE += `    ${fieldName} ${fieldType}
+        scalarFields.forEach(fieldName => {
+            const fieldType = parseClass[fieldName];
+            TABLE += `    ${fieldName} ${fieldType}
 `;
-    });
+        });
 
-    pointerFields.forEach(fieldName => {
-        const fieldType = parseClass[fieldName];
-        const pointerClass = fieldType.substring(1); // Extract `*${pointerClass}`
-        TABLE += `    ${fieldName} "Pointer (MANY-to-ONE)" [ref: > ${pointerClass}.objectId]
+        pointerFields.forEach(fieldName => {
+            const fieldType = parseClass[fieldName];
+            const pointerClass = fieldType.substring(1); // Extract `*${pointerClass}`
+            TABLE += `    ${fieldName} "Pointer (MANY-to-ONE)" [ref: > ${pointerClass}.objectId]
 `;
-        indexes.push(`      ${fieldName} [pk] // NOT pk, just for bold 😎`)
-        indexes.push(`      ${fieldName} [note: '${fieldType}'] // MANY-to-ONE`)
-    });
+            indexes.push(`      ${fieldName} [pk] // NOT pk, just for bold 😎`)
+            indexes.push(`      ${fieldName} [note: '${fieldType}'] // MANY-to-ONE`)
+        });
 
-    relationFields.forEach(fieldName => {
-        const fieldType = parseClass[fieldName];
-        const relationClass = fieldType.substring(9, fieldType.length - 1); // Extract `relation<${relationClass}>`
-        TABLE += `    ${fieldName} "Relation (MANY-to-MANY)" [ref: < ${relationClass}.objectId]
+        relationFields.forEach(fieldName => {
+            const fieldType = parseClass[fieldName];
+            const relationClass = fieldType.substring(9, fieldType.length - 1); // Extract `relation<${relationClass}>`
+            TABLE += `    ${fieldName} "Relation (MANY-to-MANY)" [ref: < ${relationClass}.objectId]
 `;
-        indexes.push(`      ${fieldName} [pk] // NOT pk, just for bold 😎`)
-        indexes.push(`      ${fieldName} [note: '${fieldType}'] // MANY-to-MANY`)
-    });
+            indexes.push(`      ${fieldName} [pk] // NOT pk, just for bold 😎`)
+            indexes.push(`      ${fieldName} [note: '${fieldType}'] // MANY-to-MANY`)
+        });
 
-    TABLE += `
+        TABLE += `
     indexes {
 ${indexes.join('\n')}
     }
 `;
 
-    TABLE += `}`;
+        TABLE += `}`;
 
-    DBML.push(TABLE);
+        DBML.push(TABLE);
 
-    // console.log(fields);
+        // console.log(fields);
 
-    classIndex += 1;
+        classIndex += 1;
 
-});
+    });
 
-const dbml = DBML.join('\n')
+    const dbml = DBML.join('\n')
 
-console.log(dbml);
+    // console.log(dbml);
 
-fs.writeFileSync('./_SCHEMA.dbml', dbml);
+    fs.writeFileSync(schemaDBML, dbml);
+}
+
+// CLI
+program.version('0.1.0');
+
+program
+    .requiredOption('-i --input <input>', 'path to the _SCHEMA.json')
+    .requiredOption('-o --output <output>', 'path to the _SCHEMA.dbml')
+    .action((env) => {
+        const schemaJSON = env.input;
+        const schemaDBML = env.output;
+
+        // console.log(input, output);
+
+        convert(schemaJSON, schemaDBML);
+    });
+
+program.parse(process.argv);
